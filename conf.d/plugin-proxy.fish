3582dccdba669bfa2ac60ab49c0496a08c554c0e
Alex <1187547+alexruzzarin@users.noreply.github.com>
2023-06-26 22:00:56 -0400

    only set proxy when needed

--- conf.d/plugin-proxy.fish
+++ conf.d/plugin-proxy.fish
@@ -1,3 +1,9 @@
 if set -q proxy_host
-  proxy
+  if set -q autoproxy_check_command
+    echo $autoproxy_check_command | source
+    set result $status
+    if test $result -eq 0
+      proxy
+    end
+  end
 end
